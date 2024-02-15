3526b87ea8cb6a74ea893faa238aaf748f2a342e
Zeeshan Gulzar <xeeshangulzar@gmail.com>
2024-02-15 15:52:04 +0500

    Asynchronously Loading HTML

---
+++ src/controllers/content_loader_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static values = { url: String }
+
+  connect() {
+    this.load()
+  }
+
+  load() {
+    fetch(this.urlValue)
+      .then(response => response.text())
+      .then(html => this.element.innerHTML = html)
+  }
+}
