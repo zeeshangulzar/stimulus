9ed5bca35404cda007465cd6d4709b8e3f478cf4
Zeeshan Gulzar <xeeshangulzar@gmail.com>
2024-02-15 15:58:29 +0500

    Refreshing Automatically With a Timer

--- src/controllers/content_loader_controller.js
+++ src/controllers/content_loader_controller.js
@@ -1,12 +1,20 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static values = { url: String }
+  static values = { url: String, refreshInterval: Number }
 
   connect() {
     this.load()
-  }
 
+    if (this.hasRefreshIntervalValue) {
+      this.startRefreshing()
+    }
+  }
+  startRefreshing() {
+    setInterval(() => {
+      this.load()
+    }, this.refreshIntervalValue)
+  }
   load() {
     fetch(this.urlValue)
       .then(response => response.text())
